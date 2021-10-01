26a9b70271bc8c92386311cbf15bf6eeaf69d352
Edmund Bates <lutemaster@gmail.com>
2021-10-01 10:49:00 -0700

    Initial Commit

---
+++ Flasht/FlashtApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashtApp.swift
+//  Flasht
+//
+//  Created by Edmund Bates on 10/1/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
